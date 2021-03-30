de6796f1996000a7276cfa199ae2ca986e976579
zhujiang2 <zhujiang2@lenovo.com>
2021-03-30 16:18:59 +0800

    删除无用依赖,减小依赖大小

--- app/src/main/java/com/zj/test/MainActivity.kt
+++ app/src/main/java/com/zj/test/MainActivity.kt
@@ -5,9 +5,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.zj.test.ui.theme.BannerTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,18 +18,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BannerTheme {
-        Greeting("Android")
     }
 }
