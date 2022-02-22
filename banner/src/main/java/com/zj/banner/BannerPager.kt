6c92658aa30fff7188b13bf3792ce8866f00b3a2
zhujiang2 <zhujiang2@lenovo.com>
2022-02-22 09:51:00 +0800

    添加滑动动画

--- banner/src/main/java/com/zj/banner/BannerPager.kt
+++ banner/src/main/java/com/zj/banner/BannerPager.kt
@@ -9,7 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.util.lerp
 import coil.annotation.ExperimentalCoilApi
 import com.google.accompanist.pager.*
 import com.zj.banner.model.BaseBannerBean
@@ -17,6 +19,7 @@ import com.zj.banner.ui.BannerCard
 import com.zj.banner.ui.config.BannerConfig
 import kotlinx.coroutines.launch
 import java.util.*
+import kotlin.math.absoluteValue
 
 private const val TAG = "BannerPager"
 
@@ -56,6 +59,29 @@ fun <T : BaseBannerBean> BannerPager(
             BannerCard(
                 bean = item,
                 modifier = Modifier
+                    .graphicsLayer {
+                        // Calculate the absolute offset for the current page from the
+                        // scroll position. We use the absolute value which allows us to mirror
+                        // any effects for both directions
+                        val pageOffset = calculateCurrentOffsetForPage(page).absoluteValue
+
+                        // We animate the scaleX + scaleY, between 85% and 100%
+                        lerp(
+                            start = 0.85f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        ).also { scale ->
+                            scaleX = scale
+                            scaleY = scale
+                        }
+
+                        // We animate the alpha, between 50% and 100%
+                        alpha = lerp(
+                            start = 0.5f,
+                            stop = 1f,
+                            fraction = 1f - pageOffset.coerceIn(0f, 1f)
+                        )
+                    }
                     .fillMaxSize()
                     .padding(config.bannerImagePadding),
                 shape = config.shape,
